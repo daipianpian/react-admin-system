2e245c9ccfd8ee4f6b5cf57800cac4a56e90d513
daipianpian <daipianpian@outlook.com>
2020-04-28 18:24:05 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {store} from './redux/index.js';
+import {CSSTransition} from "react-transition-group";
+// import Login from './views/Login/login';
+// import Index from './views/Index';
+import PrivateRoute from './router/PrivateRoute';
+// import NoMatch from './views/NoMatch';
+import history from './api/history';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router basename="/echo" history={history}>
+          <div className="entryWrap">
+              <CSSTransition
+                classNames="fade"
+                timeout={1000}
+              >
+                <Switch>
+                  // <Route path="/login" component={Login} exact/>
+                  // <PrivateRoute path="/" component={Index}/>
+                  // <Route component={NoMatch}/>
+                </Switch>
+              </CSSTransition>
+          </div>
+      </Router>
+    </Provider>
   );
 }
 
